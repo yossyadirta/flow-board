ea3726306ce3ed91d52f7f5839a0cb333d376529
Yossy Adirta <yossyadirta@gmail.com>
2026-01-23 17:33:54 +0700

    style(state): initiate tailwind and shadcn

---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: "class",
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./hooks/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
