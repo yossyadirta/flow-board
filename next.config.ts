bc6bf75bb609cb9d701e0d1ef6162bfbd6095382
Yossy Adirta <yossyadirta@gmail.com>
2026-07-27 21:29:57 +0700

    chore(app): enable image supabase hostname

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.supabase.co",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
